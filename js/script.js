a783d13ec9165877b025bcdf96c6c721e9728abb
Isabel Santiago <mariaisabelcs@live.com>
2018-08-28 20:16:58 -0300

    Adição do contador de alunos com efeito

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	// number effect
+	$('.count').each(function () {
+		$(this).prop('Counter',0).animate({
+			Counter: $(this).text()
+		}, {
+			duration: 9000,
+			easing: 'swing',
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
+	});
+});
