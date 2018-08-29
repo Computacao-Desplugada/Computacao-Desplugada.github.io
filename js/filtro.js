9909e5f606719d1e46e6aa63f61f3b998f010910
Isabel Santiago <mariaisabelcs@live.com>
2018-08-29 19:22:21 -0300

    Ajuste no menu da galeria e adição de fotos

--- js/filtro.js
+++ js/filtro.js
@@ -5,10 +5,10 @@
             var filtro = $(this).attr("data-filtro");
 
             if (filtro == "todos") {
-                $(".col-md-4").show(500);
+                $(".escola").show(500);
             } else {
-                $(".col-md-4").not("."+filtro).hide(500);
-                $(".col-md-4").filter("."+filtro).show(500);
+                $(".escola").not("."+filtro).hide(500);
+                $(".escola").filter("."+filtro).show(500);
             }
         });
         $("ul li").click(function(){
